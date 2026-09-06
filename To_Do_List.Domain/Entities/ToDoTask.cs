ac38391c79166cf92489ac38e426aebcda4b436b
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 00:33:33 +0330

    add models in project

---
+++ To_Do_List.Domain/Entities/ToDoTask.cs
@@ -0,0 +1,29 @@
+﻿
+namespace To_Do_List.Domain.Entities
+{
+    public class ToDoTask
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public Guid UserId { get; private set; }
+        public ApplicationUser ApplicationUser { get; private set; }
+
+
+        private ToDoTask() { }
+
+        public ToDoTask(string title, string description, bool isCompleted, DateTime dueDate, Guid userId)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            IsCompleted = isCompleted;
+            DueDate = dueDate;
+            IsDeleted = false;
+            UserId = userId;
+        }
+    }
+}
