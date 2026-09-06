5498db3c39f3d4a4573b5f3d59fd2f2f10abd58e
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 00:42:57 +0330

    add TaskRepository in project

---
+++ To_Do_List.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using To_Do_List.Domain.Entities;
+
+namespace To_Do_List.Domain.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<ToDoTask>> GetAllTasksAsync();
+        Task<ToDoTask> GetTaskByIdAsync(Guid id);
+        Task<ToDoTask> CreateTaskAsync(ToDoTask task);
+        Task UpdateTaskAsync(ToDoTask task);
+        Task DeleteTaskAsync(Guid id);
+    }
+}
