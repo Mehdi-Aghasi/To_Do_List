5267db581d8d0d24ddec9472b3e003527fd1b98a
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 11:24:05 +0330

    add EF Core configurations for User and Task entities

---
+++ To_Do_List.Infrastructure/Configurations/TaskConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using To_Do_List.Domain.Entities;
+
+namespace To_Do_List.Infrastructure.Configurations
+{
+    public class TaskConfiguration : IEntityTypeConfiguration<ToDoTask>
+    {
+        public void Configure(EntityTypeBuilder<ToDoTask> builder)
+        {
+            builder.ToTable("Tasks");
+            builder.HasKey(key => key.Id);
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnType("nvarchar(300)")
+                .HasMaxLength(300);
+
+            builder.Property(x => x.Description)
+                .HasColumnType("nvarchar(1000)")
+                .HasMaxLength(1000);
+
+            builder.HasOne(u => u.ApplicationUser)
+                .WithMany(t => t.ToDoTasks)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasQueryFilter(x => !x.IsDeleted);
+        }
+    }
+}
---
+++ To_Do_List.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using To_Do_List.Domain.Entities;
+
+namespace To_Do_List.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(100)
+            .HasColumnType("nvarchar(100)");
+
+            builder.Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("nvarchar(100)");
+
+            builder.Property(u => u.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
