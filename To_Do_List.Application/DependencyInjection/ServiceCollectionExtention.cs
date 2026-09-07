3bbc70d9a769c7b79b87cb4aef355f83bd160190
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 23:50:37 +0330

    implement FluentValidation pipeline behavior for MediatR

---
+++ To_Do_List.Application/DependencyInjection/ServiceCollectionExtention.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using To_Do_List.Application.Common.Behaviors;
+
+namespace To_Do_List.Application.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
