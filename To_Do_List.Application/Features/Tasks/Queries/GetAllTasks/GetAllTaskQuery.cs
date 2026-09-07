49c7119ff2230db408b6785a034adddbe9027df5
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 23:49:11 +0330

    add GetAllTasks query and handler

---
+++ To_Do_List.Application/Features/Tasks/Queries/GetAllTasks/GetAllTaskQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using To_Do_List.Application.Common.Dtos;
+
+namespace To_Do_List.Application.Features.Tasks.Queries.GetAllTasks
+{
+    public record GetAllTaskQuery() : IRequest<IEnumerable<TaskDto>>;
+}
