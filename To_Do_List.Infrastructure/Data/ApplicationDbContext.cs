eb7f31ae2674be1bb1f64676f1232790fc808611
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 11:23:37 +0330

    setup ApplicationDbContext with Identity support

---
+++ To_Do_List.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using To_Do_List.Domain.Entities;
+
+namespace To_Do_List.Infrastructure.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ToDoTask> ToDoTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(builder);
+        }
+    }
+}
