97eb9c0a2aa6c107534a820b691ab3006c5b56da
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 23:48:48 +0330

    add DeleteTask command, handler, and validator

---
+++ To_Do_List.Application/Features/Tasks/Commands/DeleteTask/DeleteTaskValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace To_Do_List.Application.Features.Tasks.Commands.DeleteTask
+{
+    public class DeleteTaskValidator:AbstractValidator<DeleteTaskCommand>
+    {
+        public DeleteTaskValidator() 
+        { 
+            RuleFor(x=>x.Id).NotEmpty().WithMessage("شناسه تسک نمی‌تواند خالی باشد");
+        }
+    }
+}
