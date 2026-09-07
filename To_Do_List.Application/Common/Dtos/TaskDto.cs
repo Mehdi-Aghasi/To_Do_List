2b1b2a3e9cb4ff2d14507077491474055413acc3
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-07 23:47:40 +0330

    add dto in project

---
+++ To_Do_List.Application/Common/Dtos/TaskDto.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace To_Do_List.Application.Common.Dtos
+{
+    public record TaskDto
+    (
+        Guid Id,
+        string Title,
+        string? Description,
+        bool IsCompleted,
+        DateTime DueDate
+    );
+}
