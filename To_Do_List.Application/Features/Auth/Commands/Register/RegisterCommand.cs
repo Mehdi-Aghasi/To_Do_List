e5372e6153e8b3235356a78cbbd86db85f567938
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-08 14:21:04 +0330

    add Register command, handler, and user creation logic

---
+++ To_Do_List.Application/Features/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using To_Do_List.Application.Common.Dtos;
+
+namespace To_Do_List.Application.Features.Auth.Commands.Register
+{
+    public record RegisterCommand
+    (
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password
+    ) : IRequest<AuthDto>;
+}
