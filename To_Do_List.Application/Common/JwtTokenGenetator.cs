d61d7647cc9a970ff2995403fb0f91dad85df1e8
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-08 14:20:37 +0330

    implement JwtTokenGenerator for JWT token creation

---
+++ To_Do_List.Application/Common/JwtTokenGenetator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using To_Do_List.Domain.Entities;
+
+namespace To_Do_List.Application.Common
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GenerateToken(ApplicationUser user, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expireDays = Convert.ToDouble(jwtSettings["ExpireDays"]);
+            var expires = DateTime.UtcNow.AddDays(expireDays);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
