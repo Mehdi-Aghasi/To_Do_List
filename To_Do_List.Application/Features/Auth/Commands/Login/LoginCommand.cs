bfb0b13c7cd5781819f0c01d57247d58f4026ebe
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-09-08 14:21:20 +0330

    add Login command, handler, and password verification

---
+++ To_Do_List.Application/Features/Auth/Commands/Login/LoginCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using To_Do_List.Application.Common.Dtos;
+
+namespace To_Do_List.Application.Features.Auth.Commands.Login
+{
+    public record LoginCommand
+    (
+        string Email,
+        string Password
+    ) : IRequest<AuthDto>;
+}
